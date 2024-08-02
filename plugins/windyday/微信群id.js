793451a79a2f9ca3b0965ccf39b5cbf981a353e1
windyday <63159472+CoverUp137@users.noreply.github.com>
2024-08-03 05:43:52 +0800

    Add files via upload

--- plugins/windyday/微信群id.js
+++ plugins/windyday/微信群id.js
@@ -6,28 +6,11 @@
  * @description 获取微信群id
  * @rule ^wx ([^ \n]+)$
  * @admin true
- * @public true
+ * @public false
  * @priority 1000
  * @disable false
  * @classification ["微信id"]
  */
- 
-module.exports = async s => {
-  
-    function utf8ToHex(inputString) {
-        const buffer = Buffer.from(inputString, 'utf-8');
-        
-        const hexString = buffer.toString('hex');
-        
 
-        return hexString;
-    }
-
-
-    const content = s.param(1);
-
-    const hexString = utf8ToHex(content);
-    await s.reply(`微信群id: ${hexString}`);
-};
-
+/** Code Encryption Block[419fd178b7a37c9eae7b7426c4a04203d45e01cd4c8344ffddd8d874f1b64a318191f9514b98d644c03366d9e6625065cd3b8db238370294e422116ecafadab76cee63a64d37dd9995f6c944670f0c7e446247b8e702391902308c60541db0a23c56f067053539e2f879daae9e27a0afcdd2346b50a89534c1221076b7e9377f129fd9b58316ee312120f96e27f8956212be9de527b765e814279e76f0ad1f959034276ff0f645e70c71f0edc74dfdd9885701dd2a0ab6a4d018dbf936782fb136e270d47a130da30c30485042940767e689f0ea27acb22b2559c71fd392c843142d7d9814ebee02df7b80d3ddf58e969c3d909dd339f2f91c06b99b1671cf5f466dd6ff73c3c356ca082186f661cdc6dde33cbdc6b367c90413392aca4745600fbee864f8dd97de0aef6e6382cb4db97c3c396a102a4ad80b262506423a0129e85f09a3f8a1503d63b4e036c5ace02cc66f05b9a98b7e7b4edd23742052fe97ffd321018c8bd047c04bd9e8da25a556f3fbbd2e7d1b08c9254773174f29bcc7817b98d8054979f84d3cfbf8e40f7697] */
 
