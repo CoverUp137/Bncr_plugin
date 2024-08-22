f71fec1efcb737e52393ece252ee147f401d7eda
windyday <63159472+CoverUp137@users.noreply.github.com>
2024-08-22 08:30:41 +0800

    Update Gitlab仓库更新记录.js

--- plugins/windyday/Gitlab仓库更新记录.js
+++ plugins/windyday/Gitlab仓库更新记录.js
@@ -3,8 +3,7 @@
  * @name Gitlab仓库更新记录
  * @team 红灯区
  * @version 1.0.4
- * @description Gitlab仓库更新记录，需bncr能爬墙
- * 命令: 查询GitLab 或 查询gitlab
+ * @description Gitlab仓库更新记录，需bncr能爬墙,命令: 查询GitLab 或 查询gitlab
  * @rule ^(查询GitLab|查询gitlab|查询Gitlab|查询GITLAB)$
  * @admin true
  * @public true
